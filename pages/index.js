78f3e48da62264d4b06ac2d1c168c94288fe6d8a
Dandun Adi <dandun.adi@gmail.com>
2021-05-19 17:19:01 +0700

    lecture #4

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard";
 
 export async function getStaticProps() {
   const client = createClient({
@@ -13,6 +14,12 @@ export async function getStaticProps() {
 }
 
 export default function Recipes({ recipes }) {
-  console.log(recipes);
-  return <div className="recipe-list">Recipe List</div>;
+  return (
+    <div className="recipe-list">
+      Recipe List
+      {recipes.map((recipe) => (
+        <RecipeCard recipe={recipe} key={recipe.sys.id} />
+      ))}
+    </div>
+  );
 }
