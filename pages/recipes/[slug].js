90c1fa6106e054602c9fe6a889d9e6cfeaa8dfc0
Dandun Adi <dandun.adi@gmail.com>
2021-05-19 18:51:18 +0700

    generate details page

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,36 @@
-export default function RecipeDetails() {
-  return (
-    <div>
-      Recipe Details
-    </div>
-  )
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export async function getStaticPaths() {
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  const paths = res.items.map((recipe) => {
+    return {
+      params: { slug: recipe.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: { recipe: items[0] },
+  };
+}
+
+export default function RecipeDetails({ recipe }) {
+  return <div>{recipe.fields.title}</div>;
 }
